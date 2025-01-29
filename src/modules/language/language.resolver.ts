34d3360a28a64955aaf47d97419fb7937f53223d
Hadi Torabi <83789371+haditorabi@users.noreply.github.com>
2025-01-29 16:04:18 -0500

    Enhance Language resolver and service to support pagination, add error handling, and update GraphQL schema for languages

--- src/modules/language/language.resolver.ts
+++ src/modules/language/language.resolver.ts
@@ -12,12 +12,16 @@ export class LanguageResolver {
   constructor(private service: LanguageService) {}
 
   @Query(() => [Language])
-  async bookingTimeSlots() {
-    return this.service.findAll();
+  async languages(
+    @Args('page', { type: () => Number, nullable: true }) page: number = 1, // Default to page 1
+    @Args('limit', { type: () => Number, nullable: true })
+    limit: number = 10, // Default page size
+  ) {
+    return this.service.findAll(page, limit);
   }
 
   @Query(() => Language)
-  async bookingTimeSlot(@Args('id') id: string) {
+  async language(@Args('id') id: string) {
     return this.service.findOne(id);
   }
 
--- src/modules/language/language.service.ts
+++ src/modules/language/language.service.ts
@@ -1,27 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { Prisma, Language } from '@prisma/client';
+import { ServiceErrorHandler } from 'src/common/decorators/ServiceErrorHandler';
 
 @Injectable()
 export class LanguageService {
   constructor(private prisma: PrismaService) {}
 
+  @ServiceErrorHandler('create language')
   async create(data: Prisma.LanguageCreateInput): Promise<Language> {
     return this.prisma.language.create({
       data,
     });
   }
 
-  async findAll(): Promise<Language[]> {
-    return this.prisma.language.findMany();
+  @ServiceErrorHandler('find all languages')
+  async findAll(page: number = 1, pageSize: number = 10): Promise<Language[]> {
+    const skip = (page - 1) * pageSize;
+
+    return this.prisma.language.findMany({
+      skip,
+      take: pageSize,
+    });
   }
 
+  @ServiceErrorHandler('find language by id')
   async findOne(id: string): Promise<Language | null> {
     return this.prisma.language.findUnique({
       where: { id },
     });
   }
 
+  @ServiceErrorHandler('update language')
   async update(
     id: string,
     data: Prisma.LanguageUpdateInput,
@@ -32,6 +42,7 @@ export class LanguageService {
     });
   }
 
+  @ServiceErrorHandler('delete language')
   async delete(id: string): Promise<Language> {
     return this.prisma.language.delete({
       where: { id },
