6c00b4be340a6e8d773a79c3d7320f189f5676aa
Hadi Torabi <83789371+haditorabi@users.noreply.github.com>
2025-01-29 14:26:07 -0500

    Enhance BusinessProduct resolver and service to support pagination, add related fields for product category, business deal, and product price, and implement error handling

--- src/modules/business-product/business-product.resolver.ts
+++ src/modules/business-product/business-product.resolver.ts
@@ -1,4 +1,11 @@
-import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Args,
+  ResolveField,
+  Parent,
+} from '@nestjs/graphql';
 import { BusinessProductService } from './business-product.service';
 import {
   BusinessProduct,
@@ -6,14 +13,22 @@ import {
   UpdateBusinessProductInput,
 } from './business-product.graphql';
 import { Prisma } from '@prisma/client';
+import { CommonProductCategory } from 'src/graphql/product-category.type';
+import { CommonBusinessDeal } from 'src/graphql/business-deal.type';
+import { BusinessProductPrice } from './business-product-price.type';
 
 @Resolver(() => BusinessProduct)
 export class BusinessProductResolver {
   constructor(private service: BusinessProductService) {}
 
   @Query(() => [BusinessProduct])
-  async businessProducts() {
-    return this.service.findAll();
+  async businessProducts(
+    @Args('page', { type: () => Number, nullable: true, defaultValue: 1 })
+    page: number,
+    @Args('pageSize', { type: () => Number, nullable: true, defaultValue: 10 })
+    pageSize: number,
+  ) {
+    return this.service.findAll(page, pageSize);
   }
 
   @Query(() => BusinessProduct)
@@ -59,5 +74,22 @@ export class BusinessProductResolver {
     };
 
     return this.service.update(id, prismaData);
+  }
+
+  // Resolve Fields for related entities
+
+  @ResolveField(() => CommonProductCategory, { nullable: true })
+  async productCategroy(@Parent() businessProduct: BusinessProduct) {
+    return this.service.getProductCategory(businessProduct.productCategroy.id);
+  }
+
+  @ResolveField(() => CommonBusinessDeal, { nullable: true })
+  async businessDeal(@Parent() businessProduct: BusinessProduct) {
+    return this.service.getBusinessDeal([businessProduct.businessDeal.id]);
+  }
+
+  @ResolveField(() => BusinessProductPrice, { nullable: true })
+  async businessProductPrice(@Parent() businessProduct: BusinessProduct) {
+    return this.service.getBusinessProductPrice(businessProduct.id);
   }
 }
--- src/modules/business-product/business-product.service.ts
+++ src/modules/business-product/business-product.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { Prisma, BusinessProduct } from '@prisma/client';
-
+import { ServiceErrorHandler } from 'src/common/decorators/ServiceErrorHandler';
 @Injectable()
 export class BusinessProductService {
   constructor(private prisma: PrismaService) {}
 
+  @ServiceErrorHandler('Create BusinessProduct')
   async create(
     data: Prisma.BusinessProductCreateInput,
   ): Promise<BusinessProduct> {
@@ -14,16 +15,23 @@ export class BusinessProductService {
     });
   }
 
-  async findAll(): Promise<BusinessProduct[]> {
-    return this.prisma.businessProduct.findMany();
+  @ServiceErrorHandler('Get All BusinessProducts')
+  async findAll(page: number, pageSize: number): Promise<BusinessProduct[]> {
+    const skip = (page - 1) * pageSize;
+    return this.prisma.businessProduct.findMany({
+      skip,
+      take: pageSize,
+    });
   }
 
+  @ServiceErrorHandler('Find BusinessProduct')
   async findOne(id: string): Promise<BusinessProduct | null> {
     return this.prisma.businessProduct.findUnique({
       where: { id },
     });
   }
 
+  @ServiceErrorHandler('Update BusinessProduct')
   async update(
     id: string,
     data: Prisma.BusinessProductUpdateInput,
@@ -34,9 +42,37 @@ export class BusinessProductService {
     });
   }
 
+  @ServiceErrorHandler('Delete BusinessProduct')
   async delete(id: string): Promise<BusinessProduct> {
     return this.prisma.businessProduct.delete({
       where: { id },
+    });
+  }
+
+  @ServiceErrorHandler('Get Product Category')
+  async getProductCategory(categoryId: string | undefined) {
+    if (!categoryId) return null;
+    return this.prisma.productCategory.findUnique({
+      where: { id: categoryId },
+    });
+  }
+
+  @ServiceErrorHandler('Get Business Deal')
+  async getBusinessDeal(dealIds: string[] | undefined) {
+    if (!dealIds || dealIds.length === 0) return null;
+    return this.prisma.businessDeal.findMany({
+      where: {
+        id: {
+          in: dealIds,
+        },
+      },
+    });
+  }
+
+  @ServiceErrorHandler('Get Business Product Price')
+  async getBusinessProductPrice(productId: string) {
+    return this.prisma.businessProductPrice.findFirst({
+      where: { businessProductId: productId },
     });
   }
 }
