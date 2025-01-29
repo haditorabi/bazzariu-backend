e8b1b66ad1ad2628bf34db0b094435c25a7580b2
Hadi Torabi <83789371+haditorabi@users.noreply.github.com>
2025-01-29 16:59:41 -0500

    Enhance Region resolver and service to support pagination, add error handling, and implement related fields for country and city

--- src/modules/region/region.resolver.ts
+++ src/modules/region/region.resolver.ts
@@ -1,15 +1,37 @@
-import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Args,
+  ResolveField,
+  Parent,
+} from '@nestjs/graphql';
 import { RegionService } from './region.service';
 import { Region, CreateRegionInput, UpdateRegionInput } from './region.graphql';
 import { Prisma } from '@prisma/client';
+import { CommonCountry } from 'src/graphql/country.type';
+import { CommonCity } from 'src/graphql/city.type';
 
 @Resolver(() => Region)
 export class RegionResolver {
   constructor(private service: RegionService) {}
 
   @Query(() => [Region])
-  async regions() {
-    return this.service.findAll();
+  async regions(
+    @Args('page', { type: () => Number, nullable: true }) page?: number,
+    @Args('limit', { type: () => Number, nullable: true }) limit?: number,
+  ) {
+    return this.service.findAll({ page, limit });
+  }
+
+  @ResolveField(() => CommonCountry)
+  async country(@Parent() region: Region) {
+    return this.service.getCountry(region.country.id);
+  }
+
+  @ResolveField(() => CommonCity)
+  async city(@Parent() region: Region) {
+    return this.service.getCity(region.city.id);
   }
 
   @Query(() => Region)
--- src/modules/region/region.service.ts
+++ src/modules/region/region.service.ts
@@ -1,27 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { Prisma, Region } from '@prisma/client';
+import { City, Country, Prisma, Region } from '@prisma/client';
+import { ServiceErrorHandler } from 'src/common/decorators/ServiceErrorHandler';
 
 @Injectable()
 export class RegionService {
   constructor(private prisma: PrismaService) {}
 
+  @ServiceErrorHandler('create region')
   async create(data: Prisma.RegionCreateInput): Promise<Region> {
     return this.prisma.region.create({
       data,
     });
   }
 
-  async findAll(): Promise<Region[]> {
-    return this.prisma.region.findMany();
+  @ServiceErrorHandler('find all regions')
+  async findAll(params: { page?: number; limit?: number }): Promise<Region[]> {
+    const { page = 1, limit = 10 } = params;
+    return this.prisma.region.findMany({
+      skip: (page - 1) * limit,
+      take: limit,
+    });
   }
 
+  @ServiceErrorHandler('find region by ID')
   async findOne(id: string): Promise<Region | null> {
     return this.prisma.region.findUnique({
       where: { id },
     });
   }
 
+  @ServiceErrorHandler('update region')
   async update(id: string, data: Prisma.RegionUpdateInput): Promise<Region> {
     return this.prisma.region.update({
       where: { id },
@@ -29,9 +38,24 @@ export class RegionService {
     });
   }
 
+  @ServiceErrorHandler('delete region')
   async delete(id: string): Promise<Region> {
     return this.prisma.region.delete({
       where: { id },
+    });
+  }
+
+  @ServiceErrorHandler('get country by ID')
+  async getCountry(countryId: string): Promise<Country> {
+    return this.prisma.country.findUnique({
+      where: { id: countryId },
+    });
+  }
+
+  @ServiceErrorHandler('get city by ID')
+  async getCity(cityId: string): Promise<City> {
+    return this.prisma.city.findUnique({
+      where: { id: cityId },
     });
   }
 }
