9b0f87e15648327a52fc57f790e0d531a3383810
Hadi Torabi <83789371+haditorabi@users.noreply.github.com>
2025-01-29 13:38:50 -0500

    Enhance BusinessBooking resolver and service to support pagination and include related data for business, products, and booking time slots

--- src/modules/business-booking/business-booking.resolver.ts
+++ src/modules/business-booking/business-booking.resolver.ts
@@ -1,4 +1,11 @@
-import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Args,
+  ResolveField,
+  Parent,
+} from '@nestjs/graphql';
 import { BusinessBookingService } from './business-booking.service';
 import {
   BusinessBooking,
@@ -6,14 +13,20 @@ import {
   UpdateBusinessBookingInput,
 } from './business-booking.graphql';
 import { Prisma } from '@prisma/client';
+import { CommonBusiness } from 'src/graphql/business.type';
+import { CommonBusinessProduct } from 'src/graphql/business-product.type';
+import { CommonBookingTimeSlot } from 'src/graphql/booking-time-slot.type';
 
 @Resolver(() => BusinessBooking)
 export class BusinessBookingResolver {
   constructor(private service: BusinessBookingService) {}
 
   @Query(() => [BusinessBooking])
-  async businessBookings() {
-    return this.service.findAll();
+  async businessBookings(
+    @Args('skip', { type: () => Number, nullable: true }) skip: number = 0,
+    @Args('take', { type: () => Number, nullable: true }) take: number = 10,
+  ) {
+    return this.service.findAll(skip, take);
   }
 
   @Query(() => BusinessBooking)
@@ -59,5 +72,21 @@ export class BusinessBookingResolver {
     };
 
     return this.service.update(id, prismaData);
+  }
+  @ResolveField(() => CommonBusiness)
+  async business(@Parent() businessBooking: BusinessBooking) {
+    return this.service.getBusiness(businessBooking.business.id);
+  }
+
+  @ResolveField(() => [CommonBusinessProduct], { nullable: 'items' })
+  async businessProduct(@Parent() businessBooking: BusinessBooking) {
+    return this.service.getBusinessProducts(
+      businessBooking.businessProduct.map((product) => product.id),
+    );
+  }
+
+  @ResolveField(() => [CommonBookingTimeSlot], { nullable: 'items' })
+  async bookingTimeSlot(@Parent() businessBooking: BusinessBooking) {
+    return this.service.getBookingTimeSlots(businessBooking.id);
   }
 }
--- src/modules/business-booking/business-booking.service.ts
+++ src/modules/business-booking/business-booking.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { Prisma, BusinessBooking } from '@prisma/client';
+import {
+  Prisma,
+  BusinessBooking,
+  Business,
+  BusinessProduct,
+  BookingTimeSlot,
+} from '@prisma/client';
+import { ServiceErrorHandler } from 'src/common/decorators/ServiceErrorHandler';
 
 @Injectable()
 export class BusinessBookingService {
   constructor(private prisma: PrismaService) {}
 
+  @ServiceErrorHandler('create business booking')
   async create(
     data: Prisma.BusinessBookingCreateInput,
   ): Promise<BusinessBooking> {
@@ -14,16 +22,35 @@ export class BusinessBookingService {
     });
   }
 
-  async findAll(): Promise<BusinessBooking[]> {
-    return this.prisma.businessBooking.findMany();
-  }
-
-  async findOne(id: string): Promise<BusinessBooking | null> {
-    return this.prisma.businessBooking.findUnique({
-      where: { id },
+  @ServiceErrorHandler('get all business bookings')
+  async findAll(
+    skip: number = 0,
+    take: number = 10,
+  ): Promise<BusinessBooking[]> {
+    return this.prisma.businessBooking.findMany({
+      skip,
+      take,
+      include: {
+        business: true, // Include business data
+        businessProduct: true, // Include associated products
+        BookingTimeSlot: true, // Include booking time slots
+      },
     });
   }
 
+  @ServiceErrorHandler('get business booking by id')
+  async findOne(id: string): Promise<BusinessBooking | null> {
+    return this.prisma.businessBooking.findUnique({
+      where: { id },
+      include: {
+        business: true, // Include business data
+        businessProduct: true, // Include associated products
+        BookingTimeSlot: true, // Include booking time slots
+      },
+    });
+  }
+
+  @ServiceErrorHandler('update business booking')
   async update(
     id: string,
     data: Prisma.BusinessBookingUpdateInput,
@@ -34,9 +61,29 @@ export class BusinessBookingService {
     });
   }
 
+  @ServiceErrorHandler('delete business booking')
   async delete(id: string): Promise<BusinessBooking> {
     return this.prisma.businessBooking.delete({
       where: { id },
+    });
+  }
+  async getBusiness(businessId: string): Promise<Business> {
+    return this.prisma.business.findUnique({
+      where: { id: businessId },
+    });
+  }
+
+  async getBusinessProducts(
+    businessProductIds: string[],
+  ): Promise<BusinessProduct[]> {
+    return this.prisma.businessProduct.findMany({
+      where: { id: { in: businessProductIds } },
+    });
+  }
+
+  async getBookingTimeSlots(bookingId: string): Promise<BookingTimeSlot[]> {
+    return this.prisma.bookingTimeSlot.findMany({
+      where: { businessBookingId: bookingId },
     });
   }
 }
