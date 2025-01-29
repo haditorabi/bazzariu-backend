fcd4a49f7dfe4a55939dbb5bb0e525b10cdfd6f7
Hadi Torabi <83789371+haditorabi@users.noreply.github.com>
2025-01-29 18:15:27 -0500

    Enhance UserFollowing resolver and service to support pagination, add error handling, and implement related fields for follower and followee

--- src/modules/user-following/user-following.resolver.ts
+++ src/modules/user-following/user-following.resolver.ts
@@ -1,4 +1,11 @@
-import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Args,
+  Parent,
+  ResolveField,
+} from '@nestjs/graphql';
 import { UserFollowingService } from './user-following.service';
 import {
   UserFollowing,
@@ -6,14 +13,18 @@ import {
   UpdateUserFollowingInput,
 } from './user-following.graphql';
 import { Prisma } from '@prisma/client';
+import { CommonUser } from 'src/graphql/user.type';
 
 @Resolver(() => UserFollowing)
 export class UserFollowingResolver {
   constructor(private service: UserFollowingService) {}
 
   @Query(() => [UserFollowing])
-  async userFollowings() {
-    return this.service.findAll();
+  async userFollowings(
+    @Args('skip', { type: () => Number, nullable: true }) skip: number = 0,
+    @Args('limit', { type: () => Number, nullable: true }) limit: number = 10,
+  ) {
+    return this.service.findAll({ skip, limit });
   }
 
   @Query(() => UserFollowing)
@@ -24,38 +35,32 @@ export class UserFollowingResolver {
   @Mutation(() => UserFollowing)
   async createUserFollowing(@Args('data') data: CreateUserFollowingInput) {
     const { follower, followee, ...rest } = data;
-
     const prismaData: Prisma.UserFollowingCreateInput = {
       ...rest,
-      followee: {
-        connect: { id: followee },
-      },
-      follower: {
-        connect: { id: follower },
-      },
+      followee: { connect: { id: followee } },
+      follower: { connect: { id: follower } },
     };
-
     return this.service.create(prismaData);
   }
 
   @Mutation(() => UserFollowing)
   async updateUserFollowing(@Args('data') data: UpdateUserFollowingInput) {
     const { id, follower, followee, ...rest } = data;
-
     const prismaData: Prisma.UserFollowingUpdateInput = {
       ...rest,
-      ...(follower && {
-        follower: {
-          connect: { id: follower },
-        },
-      }),
-      ...(followee && {
-        followee: {
-          connect: { id: followee },
-        },
-      }),
+      ...(follower && { follower: { connect: { id: follower } } }),
+      ...(followee && { followee: { connect: { id: followee } } }),
     };
-
     return this.service.update(id, prismaData);
+  }
+
+  @ResolveField(() => CommonUser)
+  async follower(@Parent() userFollowing: UserFollowing) {
+    return this.service.findUserById(userFollowing.follower.id);
+  }
+
+  @ResolveField(() => CommonUser)
+  async followee(@Parent() userFollowing: UserFollowing) {
+    return this.service.findUserById(userFollowing.followee.id);
   }
 }
--- src/modules/user-following/user-following.service.ts
+++ src/modules/user-following/user-following.service.ts
@@ -1,27 +1,39 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { Prisma, UserFollowing } from '@prisma/client';
+import { Prisma, User, UserFollowing } from '@prisma/client';
+import { ServiceErrorHandler } from 'src/common/decorators/ServiceErrorHandler';
 
 @Injectable()
 export class UserFollowingService {
   constructor(private prisma: PrismaService) {}
 
+  @ServiceErrorHandler('create UserFollowing')
   async create(data: Prisma.UserFollowingCreateInput): Promise<UserFollowing> {
-    return this.prisma.userFollowing.create({
-      data,
+    return this.prisma.userFollowing.create({ data });
+  }
+
+  @ServiceErrorHandler('findAll UserFollowings')
+  async findAll({
+    skip,
+    limit,
+  }: {
+    skip: number;
+    limit: number;
+  }): Promise<UserFollowing[]> {
+    return this.prisma.userFollowing.findMany({
+      skip,
+      take: limit,
     });
   }
 
-  async findAll(): Promise<UserFollowing[]> {
-    return this.prisma.userFollowing.findMany();
-  }
-
+  @ServiceErrorHandler('findOne UserFollowing')
   async findOne(id: string): Promise<UserFollowing | null> {
     return this.prisma.userFollowing.findUnique({
       where: { id },
     });
   }
 
+  @ServiceErrorHandler('update UserFollowing')
   async update(
     id: string,
     data: Prisma.UserFollowingUpdateInput,
@@ -32,8 +44,16 @@ export class UserFollowingService {
     });
   }
 
+  @ServiceErrorHandler('delete UserFollowing')
   async delete(id: string): Promise<UserFollowing> {
     return this.prisma.userFollowing.delete({
+      where: { id },
+    });
+  }
+
+  @ServiceErrorHandler('findUserById')
+  async findUserById(id: string): Promise<User | null> {
+    return this.prisma.user.findUnique({
       where: { id },
     });
   }
