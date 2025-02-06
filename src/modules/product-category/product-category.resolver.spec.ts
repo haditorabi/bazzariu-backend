b1bd97459d5f576cfa49741208dc3b32f6d10319
Hadi Torabi <83789371+haditorabi@users.noreply.github.com>
2025-02-06 12:02:58 -0500

    Enhance ProductCategoryResolver and ProductCategoryService: add unit tests for create, findAll, findOne, update, and delete operations; mock service dependencies and improve test coverage

--- src/modules/product-category/product-category.resolver.spec.ts
+++ src/modules/product-category/product-category.resolver.spec.ts
@@ -1,18 +1,119 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductCategoryResolver } from './product-category.resolver';
+import { ProductCategoryService } from './product-category.service';
+import {
+  ProductCategory,
+  CreateProductCategoryInput,
+  UpdateProductCategoryInput,
+} from './product-category.graphql';
+import { PaginationArgs } from 'src/graphql/pagination-args-types';
+import { ProductCategoryStatus } from '@prisma/client';
+
+// Mock data for testing
+const mockProductCategory: ProductCategory = {
+  id: '1',
+  name: 'Sample',
+  status: ProductCategoryStatus.ACTIVE,
+  createdAt: new Date(),
+};
+
+// Mock implementation of the ProductCategoryService
+const mockProductCategoryService = {
+  create: jest.fn().mockResolvedValue(mockProductCategory),
+  findAll: jest.fn().mockResolvedValue([mockProductCategory]),
+  findOne: jest.fn().mockResolvedValue(mockProductCategory),
+  update: jest.fn().mockResolvedValue(mockProductCategory),
+  delete: jest.fn().mockResolvedValue(mockProductCategory),
+};
 
 describe('ProductCategoryResolver', () => {
   let resolver: ProductCategoryResolver;
+  let service: ProductCategoryService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProductCategoryResolver],
+      providers: [
+        ProductCategoryResolver,
+        {
+          provide: ProductCategoryService,
+          useValue: mockProductCategoryService, // Inject the mocked service
+        },
+      ],
     }).compile();
 
     resolver = module.get<ProductCategoryResolver>(ProductCategoryResolver);
+    service = module.get<ProductCategoryService>(ProductCategoryService);
+  });
+  describe('createProductCategory', () => {
+    it('should create and return a product-categorys', async () => {
+      const input: CreateProductCategoryInput = {
+        ...mockProductCategory,
+      };
+
+      const result = await resolver.createProductCategory(input);
+      expect(service.create).toHaveBeenCalledWith(input);
+      expect(result).toEqual(mockProductCategory);
+    });
   });
 
-  it('should be defined', () => {
-    expect(resolver).toBeDefined();
+  describe('productCategorys (findAll)', () => {
+    it('should return an array of product-categorys', async () => {
+      const paginationArgs: PaginationArgs = {
+        skip: 0,
+        take: 10,
+        limit: 10,
+        page: 1,
+      };
+
+      // Call the resolver method
+      const result = await resolver.productCategories(paginationArgs);
+
+      // Expect the service method to have been called
+      expect(service.findAll).toHaveBeenCalledWith(paginationArgs);
+      expect(result).toEqual([mockProductCategory]);
+    });
+  });
+
+  describe('productCategory (findOne)', () => {
+    it('should return a single product-category by ID', async () => {
+      const id = '1';
+
+      // Call the resolver method
+      const result = await resolver.productCategory(id);
+
+      // Expect the service method to have been called
+      expect(service.findOne).toHaveBeenCalledWith(id);
+      expect(result).toEqual(mockProductCategory);
+    });
+  });
+
+  describe('updateProductCategory (update)', () => {
+    it('should update and return the modified product-category', async () => {
+      const id = '1';
+      const updateData = { name: 'Updated' };
+      const input: UpdateProductCategoryInput = {
+        ...updateData,
+      };
+
+      // Call the resolver method
+      const result = await resolver.updateProductCategory(id, input);
+
+      // Expect the service method to have been called with correct arguments
+      expect(service.update).toHaveBeenCalledWith(id, updateData);
+      expect(result).toEqual(mockProductCategory);
+    });
+  });
+
+  describe('deleteProductCategory (delete)', () => {
+    it('should delete a product-category and return it', async () => {
+      const id = '1';
+
+      // Call the resolver method
+      const result = await resolver.deleteProductCategory(id);
+
+      // Expect the service method to have been called
+      expect(service.delete).toHaveBeenCalledWith(id);
+      expect(result).toEqual(mockProductCategory);
+    });
   });
 });
--- src/modules/product-category/product-category.service.spec.ts
+++ src/modules/product-category/product-category.service.spec.ts
@@ -1,18 +1,126 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductCategoryService } from './product-category.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { ProductCategory } from '@prisma/client';
+import { PaginationArgs } from 'src/graphql/pagination-args-types';
+import { Prisma } from '@prisma/client';
+
+// Mock data for testing
+const mockProductCategory: ProductCategory = {
+  id: '1',
+  name: 'Sample',
+  status: 'ACTIVE', // Prisma enum value
+  createdAt: new Date(),
+};
+
+// Mock implementation of the PrismaService
+const mockPrismaService = {
+  productCategory: {
+    create: jest.fn().mockResolvedValue(mockProductCategory),
+    findMany: jest.fn().mockResolvedValue([mockProductCategory]),
+    findUnique: jest.fn().mockResolvedValue(mockProductCategory),
+    update: jest.fn().mockResolvedValue(mockProductCategory),
+    delete: jest.fn().mockResolvedValue(mockProductCategory),
+  },
+};
 
 describe('ProductCategoryService', () => {
   let service: ProductCategoryService;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProductCategoryService],
+      providers: [
+        ProductCategoryService,
+        {
+          provide: PrismaService,
+          useValue: mockPrismaService, // Inject the mocked PrismaService
+        },
+      ],
     }).compile();
 
     service = module.get<ProductCategoryService>(ProductCategoryService);
+    prisma = module.get<PrismaService>(PrismaService);
+  });
+  describe('create', () => {
+    it('should create and return a product-category', async () => {
+      const input: Prisma.ProductCategoryCreateInput = {
+        ...mockProductCategory,
+      };
+
+      const result = await service.create(input);
+      expect(prisma.productCategory.create).toHaveBeenCalledWith({
+        data: input,
+      });
+      expect(result).toEqual(mockProductCategory);
+    });
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('findAll', () => {
+    it('should return an array of product-categorys', async () => {
+      const paginationArgs: PaginationArgs = {
+        skip: 0,
+        take: 10,
+        limit: 10,
+        page: 1,
+      };
+
+      // Call the service method
+      const result = await service.findAll(paginationArgs);
+
+      // Expect the prisma method to have been called
+      expect(prisma.productCategory.findMany).toHaveBeenCalledWith({
+        skip: paginationArgs.skip,
+        take: paginationArgs.take,
+      });
+      expect(result).toEqual([mockProductCategory]);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a single product-category by ID', async () => {
+      const id = '1';
+
+      // Call the service method
+      const result = await service.findOne(id);
+
+      // Expect the prisma method to have been called
+      expect(prisma.productCategory.findUnique).toHaveBeenCalledWith({
+        where: { id },
+      });
+      expect(result).toEqual(mockProductCategory);
+    });
+  });
+
+  describe('update', () => {
+    it('should update and return the modified product-category', async () => {
+      const id = '1';
+      const updateData: Prisma.ProductCategoryUpdateInput = { name: 'Updated' };
+
+      // Call the service method
+      const result = await service.update(id, updateData);
+
+      // Expect the prisma method to have been called with correct arguments
+      expect(prisma.productCategory.update).toHaveBeenCalledWith({
+        where: { id },
+        data: updateData,
+      });
+      expect(result).toEqual(mockProductCategory);
+    });
+  });
+
+  describe('delete', () => {
+    it('should delete a product-category and return it', async () => {
+      const id = '1';
+
+      // Call the service method
+      const result = await service.delete(id);
+
+      // Expect the prisma method to have been called
+      expect(prisma.productCategory.delete).toHaveBeenCalledWith({
+        where: { id },
+      });
+      expect(result).toEqual(mockProductCategory);
+    });
   });
 });
